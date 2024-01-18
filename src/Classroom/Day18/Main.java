107d8c5bd3d3e5fadff0c2c393e84f6f4996bb3b
ulukcodewise <uluk0822@gmail.com>
2024-01-18 12:45:36 -0600

    commit

---
+++ src/Classroom/Day18/Main.java
@@ -0,0 +1,12 @@
+package Classroom.Day18;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++){
+            if(i==4){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
