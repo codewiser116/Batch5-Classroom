98388d71c6c4e9d9e6d07ab6f90fe0367df92212
ulukcodewise <uluk0822@gmail.com>
2024-02-29 12:52:21 -0600

    commit

--- src/Classroom/Day40/ReadDatesFromScanner.java
+++
@@ -1,9 +0,0 @@
-package Classroom.Day40;
-
-import java.util.Scanner;
-
-public class ReadDatesFromScanner {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
